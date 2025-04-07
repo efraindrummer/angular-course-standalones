23c6e258baf96a7e7d11d75dd671f240250780b2
efraindrummer <efrain_may_75@hotmail.com>
2025-04-07 09:04:25 -0600

    route config

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidemenu',
-  imports: [],
+  imports: [ CommonModule, RouterModule],
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css'
 })
 export default class SidemenuComponent {
 
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter(route => route && route.path)
+    .filter(route => !route.path?.includes(':'))
+
+  constructor() {
+
+    //este codigo exclute el path redireto to y las rutas con parametros
+    /* const dashboardRoutes = routes.map(
+      route => route.children ?? []
+    )
+    .flat()
+    .filter(route => route && route.path)
+    .filter(route => !route.path?.includes(':')) */
+    //console.log(dashboardRoutes)
+  }
 }
