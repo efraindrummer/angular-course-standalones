dc24a1a17e2392f8df8e0460769e4f78e09449d8
efraindrummer <efrain_may_75@hotmail.com>
2025-04-07 09:42:38 -0600

    signals

--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+type Grade = 'A' | 'B' | 'F';
 
 @Component({
-  imports: [ RouterOutlet ],
+  imports: [CommonModule ],
   templateUrl: './control-flow.component.html',
   styles: ``
 })
 export default class ControlFlowComponent {
+
+  public showContent = signal(false);
+  public grade = signal<Grade>('A');
+  public frameworks = signal(['Angular', 'React', 'React', 'Vue']);
+  public frameworks2 = signal([]);
+
+
+  public toggleContent() {
+    this.showContent.update(value => !value);
+  }
+
+
 
 }
